29773a06e2a790abeb11e31fa50b32575144584d
vinagreti <bruno@tzadi.com>
2015-03-17 14:36:08 +0000

    v0.1

--- partials/contact/contact.js
+++ partials/contact/contact.js
@@ -10,24 +10,18 @@ contact.config(['$routeProvider', function($routeProvider) {
 }]);
 
 contact.controller('ContactCtrl', ['$scope','$http', function($scope, $http) {
+
 	var ContactCtrl = this;
-	var VitrineCtrl = this;
-	ContactCtrl.contacts = [];
-    // http://104.154.82.56/api/products - URL do recurso
-    $http.get('//fodev-api-vinagreti.c9.io/api/todos').then(function(resp) {
-      ContactCtrl.contacts = resp.data;
-    }, function(err) {
-      console.error('ERR', err);
-      // err.status will contain the status code
-    });
+
     $scope.submit = function(contactForm) {
         console.log($scope);
         if (contactForm.phone.$viewValue && contactForm.email.$viewValue && contactForm.name.$viewValue && contactForm.msg.$viewValue) {
             $http.post('//fodev-api-vinagreti.c9.io/api/tickets',{
-                text: contactForm.phone.$viewValue
-                , email: contactForm.email.$viewValue
+                email: contactForm.email.$viewValue
                 , name: contactForm.name.$viewValue
+                , phone: contactForm.phone.$viewValue
                 , msg: contactForm.msg.$viewValue
+                , kind: 'contact'
             })
             .success(function(data, status, headers, config){
                 console.log(data, status, headers, config);
@@ -41,4 +35,5 @@ contact.controller('ContactCtrl', ['$scope','$http', function($scope, $http) {
         }
         return false; 
     };
+
 }]);
