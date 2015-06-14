8c17a13d07b0afee0cb5e94c391f5e892999afba
Bruno da Silva joão <bruno@tzadi.com>
2015-06-14 17:35:48 -0300

    selectFileModal done

--- partials/files/filesService.js
+++ partials/files/filesService.js
@@ -1,6 +1,15 @@
 angular.module('myApp').service('filesService', ['$modal', function ($modal) {
     
     var self = this;
+    var multiple = false;
+    
+    var setMultiple = function(mtpl){
+        multiple = mtpl || false;
+    }
+    
+    var getMultiple = function(){
+        return multiple;
+    }
     
     var callback = function(res){
         return res || '';
@@ -10,35 +19,83 @@ angular.module('myApp').service('filesService', ['$modal', function ($modal) {
         if(angular.isFunction(cbk)) callback = cbk;
     }
 
-    self.showModal = function(cbk){
+    self.selectFile = function(cbk, multiple){
+
+        setMultiple(multiple);
+
         uploadCallback(cbk);
+        
         return $modal.open({
             backdrop: true,
             keyboard: true,
             modalFade: true,
             size: 'lg',
             templateUrl: '/partials/files/files_modal.html',
-            controller: filesUploadFileModalCtrl
+            controller: filesModalCtrl
         }).result;
     };
     
-    var filesUploadFileModalCtrl = function (Upload, $scope, $http, $modalInstance, myConfig) {
+    var filesModalCtrl = function (Upload, $scope, $http, $modalInstance, myConfig, $filter) {
         
         $scope.progress = 4;
         $scope.uploading = false;
-    
-        $scope.setFiles = function(files){
-            $scope.files = files;
+        $scope.audios = [];
+        $scope.videos = [];
+        $scope.images = [];
+        $scope.file = {};
+        $scope.selectedItems = [];
+
+        $scope.prepareModalContent = function(fileType){
+            
+            if(['images', 'videos', 'audios'].indexOf(fileType) > -1){
+                
+                if($scope[fileType].length == 0){
+                    
+                    $http.get(myConfig.apiUrl+'/files/'+fileType)
+                    .success(function(res) {
+                        
+                        $scope[fileType] = res;
+                        
+                    }).error(function(err) {
+                    
+                        console.error('ERR', err);
+                    
+                    });
+                    
+                }
+                
+            }
+            
         }
-    
+
         $scope.modalOptions = {
             ok: function (result) {
-                $modalInstance.dismiss('order_review');
+                var items = getMultiple() ? $scope.selectedItems : $scope.selectedItems[0];
+                callback(items);
+                $scope.selectedItems
+                $modalInstance.dismiss('files_selected');
             },
             close: function (result) {
                 $modalInstance.dismiss('cancel');
             }
         };
+        
+        $scope.selectFile = function(file, finishSelection){
+            
+            if(getMultiple()){
+                $scope.selectedItems.push(file);
+            } else {
+                $scope.selectedItems = [file];
+                if(finishSelection){
+                    $scope.modalOptions.ok();
+                }
+            }
+        }
+        
+        $scope.isSelectedFile = function(file){
+            var elementPos = $scope.selectedItems.map(function(x) {return x._id; }).indexOf(file._id);
+            return elementPos > -1 ? true : false;
+        }
         
         $scope.upload = function (files) {
             
@@ -96,8 +153,6 @@ angular.module('myApp').service('filesService', ['$modal', function ($modal) {
         
         var persistImage = function(s3SignRes, file){
             
-            console.log(file);
-    
             $http.post(myConfig.apiUrl+'/file', {
                 name: $scope.name
                 , file_name: file.name
@@ -106,6 +161,8 @@ angular.module('myApp').service('filesService', ['$modal', function ($modal) {
                 , url: s3SignRes.url
             })
             .success(function(res) {
+                
+                $modalInstance.dismiss('files_selected');
                 
                 callback(res);
                 
